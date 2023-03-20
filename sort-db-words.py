6e5fe3db95789b08d8c1d385e156c10f3dd77e8d
Jeremy-Gstein <jeremy51b5@protonmail.com>
2023-03-20 07:28:19 +0000

    remove timer.

--- sort-db-words.py
+++ sort-db-words.py
@@ -1,16 +1,6 @@
 import redis
 import time
 
-start_time = 0.0
-
-def get_t():
-    global start_time
-    start_time = time.time()
-def end_t():
-    endTime = time.time()
-    total = endTime - start_time
-    print(f"Elapsed time: {total}")
-get_t()
 # Connect to Redis (Input later, leave in code for now.)
 r = redis.Redis(host='10.0.0.22', port=6379, db=0)
 
@@ -18,24 +8,22 @@ r = redis.Redis(host='10.0.0.22', port=6379, db=0)
 # Redis Data is stored as arbirarty len sentences of text.
 # Need to get ALL sets (set_N) / set_1, set_2..etc.
 set_keys = r.keys('set_*') # Currently takes ~40 seconds to enummerate all existing set keys.
-end_t()
-get_t()
+
 # Combine all sets into a single union set (with scores)
 union_set = r.sunion(set_keys)
-end_t()
-get_t()
+print('[+] Connected to Redis, Generating Set List...')
 # Get Numbers, concat to single string and add to list below.
 removeNumbers = ''
 for num in range(10):
     removeNumbers += str(num)
 # Sort the union set by splitting each value into individual words
 charsToRemove = ['.', ',', '!', '?','"','_','$','#','@','%','^','&','*','(',')','[',']',';', ':',"'",'-','/','>','<','`',removeNumbers, ]
+print(charsToRemove)
 removeChars = "".join(charsToRemove).encode('utf-8') 
-end_t()
-get_t()
 # Concat list of chars to remove and save to str var. Store as a bytes object in utf-8 format.
 # Create a List to store individual unique words.
 # Normilize all chars to lowercase and remove unwanted chars before appending to list.
+print("[+] Creating Sorted Set")
 sorted_set = []
 for value in union_set:
     words = value.split()
@@ -43,8 +31,8 @@ for value in union_set:
         trimedWord = word.translate(None, removeChars) #Arg needs to be bytes value
         sorted_set.append(trimedWord.lower())
 sorted_set.sort()
-end_t()
-get_t()
+
+print('[+] Creating Frequency Dict.')
 # Create a Dict to store Word: Freq. 
 freq_dict = {}
 for word in sorted_set:
@@ -52,19 +40,16 @@ for word in sorted_set:
         freq_dict[word] += 1
     else:
         freq_dict[word] = 1
-end_t()
-get_t()
+
 # Create 2 itterators. Word will be key and freq is value. (Member and Score for sorted_set)
 for word, freq in freq_dict.items():
     r.zadd('word_freq', {word: freq})
-end_t()
-get_t()
-word_freq = r.zrange('word_freq', 0, -1, withscores=True)
-end_t()
-get_t()
+
+# word_freq = r.zrange('word_freq', 0, -1, withscores=True)
+
 # Temp work around for None type value.
 r.zrem('word_freq', b'')
-end_t()
+
 # <---Testing--->
 '''
 for word, freq in word_freq:
@@ -72,21 +57,20 @@ for word, freq in word_freq:
 '''
 
 
+# todo: REMOVE OR FIX 
+'''
+timer_total = 0.0
+# init timer (end rest)
+start_time = time.time()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# Function for a timer for testing.
+# Global var is 'timer_total' this keeps track of time. (start_timer = time.time() is needed outside scope)
+# Get the current time and subtract start (0.0) and total ()
+def end_t():
+global timer_total
+endTime = time.time()
+timer_total = endTime - start_time - timer_total
+print(f"Elapsed Time Since Last Call: { round(timer_total, 5) } ")
+'''
 
